3bae9fd686f0e6ddfa2b5a47fa06f38310ee2c55
Michael Wu <michaelwu@palantir.com>
2018-07-02 18:17:04 -0400

    Add -s and -t options
    Adds the -s, --simple-output-name and -t, --trim options that
    respectively don't append sheet names to output names for single-sheet
    workbooks and trim empty cells from the end of each row.

--- src/xls2csv.ts
+++ src/xls2csv.ts
@@ -1,23 +1,77 @@
+import * as program from "commander";
 import * as fs from "fs";
 import * as path from "path";
-import { readFile, utils } from "xlsx";
+import { Readable } from "stream";
+import * as stream from "stream";
+import * as wrap from "word-wrap";
+import { readFile, Sheet, stream as xlsxStream } from "xlsx";
 
-const usage = "usage: xls2csv filename";
+let usage = [
+    "Converts Excel files (.xls, .xlsx) into CSV files by the same name. Outputs are placed in the current working",
+    "directory and derive their name from the workbook and sheet names.",
+    "\n\nFor example, given an Excel file of name 'foobar.xls' with sheets 'A' and 'B', the output files would be",
+    "named 'foobar_A.csv' and 'foobar_B.csv'.",
+].reduce((prev, curr) => prev + " " + curr);
+usage = wrap(usage, { width: 80, indent: "", newline: "\n  " });
 
-const args = process.argv;
-if (args.length !== 3) {
-    console.error(usage);
+program
+    .usage("[options] excel_file1 ...")
+    .description(usage)
+    .option("-s, --simple-output-name", "Do not append sheet name to output name for single-sheet workbooks.")
+    .option("-t, --trim", "Trim empty cells from the end of each row.")
+    .parse(process.argv);
+
+if (program.args.length < 1) {
+    // Must specify at least one input file.
+    program.outputHelp();
     process.exit(1);
 }
-const filename = args[2];
-const baseOutFilename = filename.replace(/\.[^/.]+$/, "");
 
-const workbook = readFile(path.join(process.cwd(), filename));
-for (const sheetName of workbook.SheetNames) {
-    const sheet = workbook.Sheets[sheetName];
-    const csv = utils.sheet_to_csv(sheet).replace(/[,]{2,}/gm, "");
-    const outFilename = workbook.SheetNames.length === 1
+for (const filePath of program.args) {
+    toCsv(filePath, { simpleOutputName: program.simpleOutputName, trimEmptyCells: program.trim });
+}
+
+export interface IOptions {
+    simpleOutputName: boolean;
+    trimEmptyCells: boolean;
+}
+
+export function toCsv(filePath: string, options: IOptions) {
+    const baseOutFilename = path.basename(filePath).replace(/\.[^/.]+$/, "");
+    const workbook = readFile(path.resolve(filePath));
+    const promises: Array<Promise<void>> = [];
+    for (const sheetName of workbook.SheetNames) {
+        const outFilePath = path.resolve(
+            process.cwd(),
+            getOutFilename(baseOutFilename, sheetName, workbook.SheetNames, options),
+        );
+        const sheet = workbook.Sheets[sheetName];
+        promises.push(streamToCsv(sheet, outFilePath, options));
+    }
+}
+
+function streamToCsv(sheet: Sheet, outFilePath: string, options: IOptions) {
+    return new Promise<void>((resolve, reject) => {
+        const readStream = xlsxStream.to_csv(sheet) as Readable;
+        const transformStream = new stream.Transform();
+        transformStream._transform = (chunk, _encoding, _callback) => {
+            if (options.trimEmptyCells) {
+                transformStream.push(chunk.replace(/[,]{2,}$/gm, ""));
+            } else {
+                transformStream.push(chunk);
+            }
+        };
+        const writeStream = fs.createWriteStream(outFilePath, { flags: "a+" });
+
+        [readStream, transformStream, writeStream].forEach((someStream) => someStream.on("error", reject));
+        writeStream.on("close", () => resolve());
+
+        readStream.pipe(transformStream).pipe(writeStream);
+    });
+}
+
+function getOutFilename(baseOutFilename: string, sheetName: string, sheetNames: string[], option: IOptions) {
+    return sheetNames.length === 1 && option.simpleOutputName
         ? baseOutFilename + ".csv"
         : baseOutFilename + "_" + sheetName + ".csv";
-    fs.writeFile(path.join(process.cwd(), outFilename), csv, undefined, () => { /* do nothing */ });
 }
